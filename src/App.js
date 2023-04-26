17fcc6a061b3ee66b352f3e1192dd408ddc15718
Samuel Odigie <samodigie98@gmail.com>
2023-04-26 16:21:15 +0100

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="container mx-auto">
-      <h1 className="text-2xl font-bold mb-4">C++ Flash Cards</h1>
+      <h1 className="text-2xl font-bold mb-4">C# Flash Cards</h1>
       <FlashCard />
     </div>
   );
